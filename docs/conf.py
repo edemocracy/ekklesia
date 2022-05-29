9ee79948865ff44ce1dcfa16c29bbd97055b4599
Tobias dpausp <dpausp@posteo.de>
2022-05-29 21:02:26 +0000

    Version 22.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -18,11 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = "Ekklesia"
-copyright = "2021, Ekklesia contributors"
+copyright = "2022, Ekklesia contributors"
 author = "Ekklesia contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "21.9.0"
+release = "22.5.0"
 
 
 # -- General configuration ---------------------------------------------------
