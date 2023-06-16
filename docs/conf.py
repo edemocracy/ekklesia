bc81ac433d0eefb16663f608e6458713f4440504
Tobias dpausp <dpausp@posteo.de>
2023-06-16 00:07:49 +0000

    Version 23.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Ekklesia contributors"
 author = "Ekklesia contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "23.04.0"
+release = "23.06.0"
 
 
 # -- General configuration ---------------------------------------------------
